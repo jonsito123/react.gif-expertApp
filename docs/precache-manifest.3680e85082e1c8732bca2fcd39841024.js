e89f9f1fd9dcac8ff7b756da05fb7603044a2aaf
Edwin Cueva <ecuevah16_1@unc.edu.pe>
2021-07-16 11:06:22 +0200

    docs agregados

---
+++ docs/precache-manifest.3680e85082e1c8732bca2fcd39841024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d670e323f0977dea87b140a82785400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9b02431bc71ccfc325",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9b02431bc71ccfc325",
+    "url": "/static/js/main.4901df1f.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
